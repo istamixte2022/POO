524ec5c383d15bc440c677b5653c666d34d0f331
istamixte2022 <hamidallahinc@gmail.com>
2023-01-04 00:06:57 +0100

    adding more instances variable

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ class Employee:
     company_name = 'ECMA Company'
 
     # constructor to initialize the object
-    def __init__(self, name, salary):
+    def __init__(self, name, salary, code):
         # instance variables
         self.name = name
         self.salary = salary
+        self.code = code
 
     # instance method
     def show(self):
-        print('Employee:', self.name, self.salary, self.company_name)
+        print('Employee:', self.name, self.salary, self.code, self.company_name)
 
 # create first object
-emp1 = Employee("Harry", 12000)
+emp1 = Employee("Harry", 12000, "A2023")
 emp1.show()
 
 # create second object
-emp2 = Employee("Emma1", 10000)
+emp2 = Employee("Emma1", 10000, "A2254")
 emp2.show()
